8219be9290d7e55c1a4205ace1039787eb09494d
Sergio Bastos <sergiobastosfisio@gmail.com>
2025-02-25 11:21:09 -0300

    teste novo deploy

--- server/src/config/database.config.ts
+++ server/src/config/database.config.ts
@@ -1,5 +1,8 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as dotenv from 'dotenv';
+import { Cliente } from 'src/modules/clientes/entities/cliente.entity/cliente.entity';
+import { Endereco } from 'src/modules/clientes/entities/endereco.entity/endereco.entity';
+import { Telefone } from 'src/modules/clientes/entities/telefone.entity/telefone.entity';
 
 dotenv.config();
 
@@ -10,6 +13,10 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  entities: [Cliente, Telefone, Endereco],
+  synchronize: true,
   autoLoadEntities: true,
-  synchronize: true, // Em produção, use migrations!
 };
